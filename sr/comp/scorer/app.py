5d123fedaa6c3090a01218b63f2c7ef5086a6488
Peter Law <PeterJCLaw@gmail.com>
2017-08-02 21:39:25 +0100

    Colour the zone labels
    Change-Id: I00dd4fc86f3bf1d34fceeddc7743bc091fc54dc8

--- sr/comp/scorer/app.py
+++ sr/comp/scorer/app.py
@@ -195,6 +195,7 @@ def update(arena, num):
 
     template_settings = {'match': match,
                          'arenas': comp.arenas,
+                         'corners': comp.corners,
                          'teams': comp.teams}
 
     if flask.request.method == 'GET':
