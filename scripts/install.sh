b7ef436fb0e810b4eb004aef374ff2bf5fa069f2
Peter Law <PeterJCLaw@gmail.com>
2019-05-25 14:28:48 +0100

    Add some deployment scripts
    `run-scorer.sh` was developed and used at SR2019. The `install.sh`
    is actually untested, but is essentially what I did at SR2019 to
    configure the scorer machine.

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#python3 -m venv $HOME/srcomp-venv
+
+#. $HOME/srcomp-venv/bin/activate
+
+#MY_DIR=$(dirname $0)
+
+#pip install -r $MY_DIR/requirements.txt
+
+#cp $MY_DIR/run-scorer.sh $HOME
+
+echo "SRComp Scorer installed."
+echo
+echo "Run the scorer via '~/run-scorer.sh'."
+echo "That will launch the scorer and open the UI in Firefox."
+echo
+echo "You may also need to configure ssh access to the scorer machine"
+echo "and install a suitable public key."
