9d1e7a5057dabe89fe635a2b41b60dd6168cdd2f
Peter Law <PeterJCLaw@gmail.com>
2020-08-16 16:42:19 +0100

    Extract a (dynamic) Converter API

--- sr/comp/scorer/app.py
+++ sr/comp/scorer/app.py
@@ -4,6 +4,8 @@ import itertools
 import os
 import sys
 from datetime import datetime
+from pathlib import Path
+from typing import Type
 
 import dateutil.tz
 import flask
@@ -62,69 +64,78 @@ def is_match_done(match):
     return os.path.exists(path)
 
 
-def form_to_score(match, form):
+class Converter:
+    @staticmethod
+    def form_team_to_score(form, zone_id):
+        return {
+            'zone': zone_id,
+            'disqualified':
+                form.get('disqualified_{}'.format(zone_id), None) is not None,
+            'present':
+                form.get('present_{}'.format(zone_id), None) is not None,
+        }
 
-    def form_team_to_score(zone_id, teams):
-        tla = form.get('tla_{}'.format(zone_id), None)
-        if tla:
-            team = {
-                'zone': zone_id,
-                'disqualified':
-                    form.get('disqualified_{}'.format(zone_id), None) is not None,
-                'present':
-                    form.get('present_{}'.format(zone_id), None) is not None,
-            }
+    @classmethod
+    def form_to_score(cls, match, form):
+        zone_ids = range(len(match.teams))
 
-            teams[tla] = team
+        teams = {}
+        for zone_id in zone_ids:
+            tla = form.get('tla_{}'.format(zone_id), None)
+            if tla:
+                teams[tla] = cls.form_team_to_score(form, zone_id)
 
-    zone_ids = range(len(match.teams))
+        zones = list(zone_ids) + ['other']
+        arena = {}
+        for zone in zones:
+            arena[zone] = {'tokens': form.get('tokens_{}'.format(zone), '')}
 
-    teams = {}
-    for zone_id in zone_ids:
-        form_team_to_score(zone_id, teams)
+        return {
+            'arena_id': match.arena,
+            'match_number': match.num,
+            'teams': teams,
+            'arena_zones': arena,
+        }
 
-    zones = list(zone_ids) + ['other']
-    arena = {}
-    for zone in zones:
-        arena[zone] = {'tokens': form.get('tokens_{}'.format(zone), '')}
+    @staticmethod
+    def score_to_form(score):
+        form = {}
 
-    return {
-        'arena_id': match.arena,
-        'match_number': match.num,
-        'teams': teams,
-        'arena_zones': arena,
-    }
-
-
-def score_to_form(score):
-    form = {}
-
-    for tla, info in score['teams'].items():
-        zone_id = info['zone']
-        form['tla_{}'.format(zone_id)] = tla
-        form['disqualified_{}'.format(zone_id)] = info.get('disqualified', False)
-        form['present_{}'.format(zone_id)] = info.get('present', True)
-
-    for zone, info in score['arena_zones'].items():
-        form['tokens_{}'.format(zone)] = info['tokens'].upper()
-
-    return form
-
-
-def match_to_form(match):
-    form = {}
-
-    for zone_id, tla in enumerate(match.teams):
-        if tla:
+        for tla, info in score['teams'].items():
+            zone_id = info['zone']
             form['tla_{}'.format(zone_id)] = tla
-            form['disqualified_{}'.format(zone_id)] = False
-            form['present_{}'.format(zone_id)] = False
+            form['disqualified_{}'.format(zone_id)] = info.get('disqualified', False)
+            form['present_{}'.format(zone_id)] = info.get('present', True)
 
-        form['tokens_{}'.format(zone_id)] = ''
+        for zone, info in score['arena_zones'].items():
+            form['tokens_{}'.format(zone)] = info['tokens'].upper()
 
-    form['tokens'] = ''
+        return form
 
-    return form
+    @staticmethod
+    def match_to_form(match):
+        form = {}
+
+        for zone_id, tla in enumerate(match.teams):
+            if tla:
+                form['tla_{}'.format(zone_id)] = tla
+                form['disqualified_{}'.format(zone_id)] = False
+                form['present_{}'.format(zone_id)] = False
+
+            form['tokens_{}'.format(zone_id)] = ''
+
+        form['tokens'] = ''
+
+        return form
+
+
+def load_converter(compstate_path: Path) -> Type[Converter]:
+    """
+    Load the score coverter module from Compstate repo.
+
+    :param Path compstate_path: The path to the compstate repo.
+    """
+    return Converter
 
 
 def update_and_validate(compstate, match, score, force):
@@ -206,6 +217,8 @@ def update(arena, num):
     compstate = flask.g.compstate
     comp = compstate.load()
 
+    converter = load_converter(comp.root)
+
     try:
         match = comp.schedule.matches[num][arena]
     except (IndexError, KeyError):
@@ -222,12 +235,12 @@ def update(arena, num):
         try:
             score = compstate.load_score(match)
         except IOError:
-            flask.request.form = match_to_form(match)
+            flask.request.form = converter.match_to_form(match)
         else:
-            flask.request.form = score_to_form(score)
+            flask.request.form = converter.score_to_form(score)
     elif flask.request.method == 'POST':
         try:
-            score = form_to_score(match, flask.request.form)
+            score = converter.form_to_score(match, flask.request.form)
         except ValueError as e:
             return flask.render_template(
                 'update.html',
