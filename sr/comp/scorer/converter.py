43d818c0b56ed7c249fd99b2ba2b54412cd9bbd6
Peter Law <PeterJCLaw@gmail.com>
2024-02-24 11:12:02 +0000

    Ease extension of the Converter through types and more hooks
    This pulls in much more of srcomp's typing to clarify the types
    being used here and extracts more hooks to aid both with typing
    and extension of this base. I'd still like to get to a point
    where much more of this is configurable rather than needing
    fully spelling out, but this is still a useful step.

--- sr/comp/scorer/converter.py
+++ sr/comp/scorer/converter.py
@@ -4,9 +4,15 @@ import copy
 import imp
 import sys
 from pathlib import Path
-from typing import cast, Type
+from typing import cast, NewType, Type, Union
 
 from sr.comp.match_period import Match
+from sr.comp.types import ScoreArenaZonesData, ScoreData, ScoreTeamData, TLA
+
+ZoneId = int
+Zone = Union[int, str]
+InputForm = NewType('InputForm', dict[str, str])
+OutputForm = NewType('OutputForm', dict[str, Union[str, bool, int, None]])
 
 
 class Converter:
@@ -14,7 +20,7 @@ class Converter:
     Base class for converting between representations of a match's score.
     """
 
-    def form_team_to_score(self, form, zone_id):
+    def form_team_to_score(self, form: InputForm, zone_id: ZoneId) -> ScoreTeamData:
         """
         Prepare a team's scoring data for saving in a score dict.
 
@@ -28,7 +34,17 @@ class Converter:
                 form.get(f'present_{zone_id}', None) is not None,
         }
 
-    def form_to_score(self, match, form):
+    def form_zone_to_score(self, form: InputForm, zone: Zone) -> ScoreArenaZonesData:
+        """
+        Prepare a zone's scoring data for saving in a score dict.
+
+        This is data which relates to the state of the zone itself.
+        """
+        return ScoreArenaZonesData({
+            'tokens': form.get(f'tokens_{zone}', ''),
+        })
+
+    def form_to_score(self, match: Match, form: InputForm) -> ScoreData:
         """
         Prepare a score dict for the given match and form dict.
 
@@ -41,47 +57,61 @@ class Converter:
         for zone_id in zone_ids:
             tla = form.get(f'tla_{zone_id}', None)
             if tla:
-                teams[tla] = self.form_team_to_score(form, zone_id)
+                teams[TLA(tla)] = self.form_team_to_score(form, zone_id)
 
         zones = list(zone_ids) + ['other']
-        arena = {}
-        for zone in zones:
-            arena[zone] = {'tokens': form.get(f'tokens_{zone}', '')}
+        arena = ScoreArenaZonesData({
+            zone: self.form_zone_to_score(form, zone)
+            for zone in zones
+        })
 
-        return {
+        return ScoreData({
             'arena_id': match.arena,
             'match_number': match.num,
             'teams': teams,
             'arena_zones': arena,
-        }
+        })
 
-    def score_to_form(self, score):
+    def score_team_to_form(self, tla: TLA, info: ScoreTeamData) -> OutputForm:
+        zone_id = info['zone']
+        return OutputForm({
+            f'tla_{zone_id}': tla,
+            f'disqualified_{zone_id}': info.get('disqualified', False),
+            f'present_{zone_id}': info.get('present', True),
+        })
+
+    def score_zone_to_form(self, zone: int | str, zone_info: ScoreArenaZonesData) -> OutputForm:
+        # TODO: use generics to avoid this cast?
+        info = cast(dict[str, str], zone_info)
+        return OutputForm({
+            f'tokens_{zone}': info['tokens'].upper(),
+        })
+
+    def score_to_form(self, score: ScoreData) -> OutputForm:
         """
         Prepare a form dict for the given score dict.
 
         This method is used when there is an existing score for a match.
         """
-        form = {}
+        form = OutputForm({})
 
-        for tla, info in score['teams'].items():
-            zone_id = info['zone']
-            form[f'tla_{zone_id}'] = tla
-            form[f'disqualified_{zone_id}'] = info.get('disqualified', False)
-            form[f'present_{zone_id}'] = info.get('present', True)
+        for tla, team_info in score['teams'].items():
+            form.update(self.score_team_to_form(tla, team_info))
 
-        for zone, info in score.get('arena_zones', {}).items():
-            form[f'tokens_{zone}'] = info['tokens'].upper()
+        # arena_zones isn't usefully typed; see https://github.com/PeterJCLaw/srcomp/issues/28
+        for zone, zone_info in score.get('arena_zones', {}).items():  # type: ignore[union-attr]
+            form.update(self.score_zone_to_form(zone, zone_info))
 
         return form
 
-    def match_to_form(self, match: Match) -> dict[str, str | bool]:
+    def match_to_form(self, match: Match) -> OutputForm:
         """
         Prepare a fresh form dict for the given match.
 
         This method is used when there is no existing score for a match.
         """
 
-        form: dict[str, str | bool] = {}
+        form = OutputForm({})
 
         for zone_id, tla in enumerate(match.teams):
             if tla:
